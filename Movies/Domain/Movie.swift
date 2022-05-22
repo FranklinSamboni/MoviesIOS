d0b5298c924124e598c5eeb3dac60a2531a27691
Franklin Samboni Castillo <sambonifranklin@gmail.com>
2022-05-22 13:49:55 -0500

    - Add uni test target
    - Add domain model
    - Remove default iOS composition

---
+++ Movies/Domain/Movie.swift
@@ -0,0 +1,21 @@
+//
+//  Movie.swift
+//  Movies
+//
+//  Created by Franklin Samboni on 22/05/22.
+//
+
+import Foundation
+
+struct Movie {
+    let id: Int
+    let adult: Bool
+    let title: String
+    let overview: String
+    let backdropImage: String
+    let posterImage: String
+    let voteAverage: Float
+    let voteCount: Int
+    let releaseDate: Date
+    let popularity: Double
+}
